d34e7396345c8786d1aec412566608c80ec79036
leeal <leeal111@163.com>
2024-04-10 17:36:56 +0800

    修改了交叉编译变量并新增了window下的预处理定义

--- cmake/arm_linux_setup.cmake
+++ cmake/arm_linux_setup.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(CROSS_COMPILE_TOOL_PATH /home/wh/work/cross_compile/gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf)
-set(CMAKE_C_COMPILER ${CROSS_COMPILE_TOOL_PATH}/bin/arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER ${CROSS_COMPILE_TOOL_PATH}/bin/arm-linux-gnueabihf-g++)
+set(CROSS_COMPILE_TOOL_PATH /home/leeal/workspace/bitSTIV/thirdparty/crosscompile)
+set(CMAKE_C_COMPILER ${CROSS_COMPILE_TOOL_PATH}/bin/aarch64-none-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER ${CROSS_COMPILE_TOOL_PATH}/bin/aarch64-none-linux-gnu-g++)
