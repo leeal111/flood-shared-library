1d9ad29930ea41c924778f1dd9e6c1f84910c414
leeal <leeal111@163.com>
2024-04-10 17:48:17 +0800

    修改注释

--- include/bitSTIV.h
+++ include/bitSTIV.h
@@ -41,7 +41,7 @@ extern "C"
      * @brief 求解给定STI图像的分数
      * 
      * @param ifR2L 指定水流方向，如果为从左往右流则输入0，如果从右往左流则输入1
-     * @param path 指定水流方向，如果为从左往右流则输入0，如果从右往左流则输入1
+     * @param path STI图像的路径
      * @return double 0-1范围内的浮点小数，即图像分数结果
      */
     double DLL_DECL_EXPORT BIT_sti2score_path(int ifR2L, const char *path);
