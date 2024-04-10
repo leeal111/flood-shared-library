506361cacad273d1b61b6e8038a6cad2e2a1df6f
leeal <leeal111@163.com>
2024-04-10 17:44:27 +0800

    补充函数导出签名

--- include/bitSTIV.h
+++ include/bitSTIV.h
@@ -35,7 +35,7 @@ extern "C"
      * @param path STI图像的路径
      * @return double 以角度制为单位的浮点数，即纹理角度结果
      */
-    double BIT_sti2angle_path(int ifR2L, const char *path);
+    double DLL_DECL_EXPORT BIT_sti2angle_path(int ifR2L, const char *path);
 
     /**
      * @brief 求解给定STI图像的分数
@@ -44,7 +44,7 @@ extern "C"
      * @param path 指定水流方向，如果为从左往右流则输入0，如果从右往左流则输入1
      * @return double 0-1范围内的浮点小数，即图像分数结果
      */
-    double BIT_sti2score_path(int ifR2L, const char *path);
+    double DLL_DECL_EXPORT BIT_sti2score_path(int ifR2L, const char *path);
 #ifdef __cplusplus
 }
 #endif
